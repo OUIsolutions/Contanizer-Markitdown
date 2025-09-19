20c9e2d49f5bb9d81c52197e8bef390bbbb9987a
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2025-09-19 17:45:04 -0300

    att

--- markitdown.lua
+++ markitdown.lua
@@ -11,7 +11,7 @@ if action == "set_provider" then
 end
 
 -- Create a new container machine
-local image = ship.create_machine("alpine:latest")
+local image = ship.create_machine("python:3.13-slim-bullseye")
 
 -- Configure container runtime
 image.provider = get_prop("markitdown.provider")
@@ -20,10 +20,10 @@ if not image.provider or image.provider == "" then
 end
 
 -- Add build-time commands
-image.add_comptime_command("apk update")
-image.add_comptime_command("apk add git")
-image.add_comptime_command("apk add python3")
-image.add_comptime_command("apk add py3-pip")
+image.add_comptime_command("apt-get update")
+image.add_comptime_command("apt-get install -y git")
+image.add_comptime_command("apt-get install -y python3")
+image.add_comptime_command("apt-get install -y python3-pip")
 
 image.add_comptime_command("git clone https://github.com/microsoft/markitdown.git")
 image.add_comptime_command("cd markitdown && pip3 install -e 'packages/markitdown[all]' --break-system-packages")
