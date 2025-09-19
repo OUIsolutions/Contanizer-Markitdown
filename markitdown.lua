61cfc0968646cad8ed9b19c77f4166e4788d6ea3
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2025-09-19 17:31:22 -0300

    att

--- markitdown.lua
+++ markitdown.lua
@@ -16,15 +16,17 @@ local image = ship.create_machine("alpine:latest")
 -- Configure container runtime
 image.provider = get_prop("markitdown.provider")
 if not image.provider or image.provider == "" then
-    print("Provider not set. use markitdown.set_provider <docker | podman>")
+    print("Provider not set. use markitdown set_provider <docker | podman>")
 end
 
 -- Add build-time commands
 image.add_comptime_command("apk update")
 image.add_comptime_command("apk add git")
-
+image.add_comptime_command("apk add python3")
+image.add_comptime_command("apk add py3-pip")
+image.add_comptime_command("git clone https://github.com/microsoft/markitdown.git")
+image.add_comptime_command("cd markitdown && pip3 install -e 'packages/markitdown[all]' --break-system-packages")
 -- Copy files
-image.copy("source.c", "source.c")
 
 -- Start container with specific configuration
 image.start({
